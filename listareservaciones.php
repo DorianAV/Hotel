5cce4a18b0792964646806fcabcff2cac587c7f1
Dorian Cinue Arce Vázquez <doriancinue26@gmail.com>
2024-04-14 22:55:03 -0600

    1

--- listareservaciones.php
+++ listareservaciones.php
@@ -2,8 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 $id = $_GET['ClvUsu'];
 $sql = "SELECT * FROM reservaciones WHERE ClvUsu='$id'";
-$con = mysqli_connect('localhost','root','2004
-','proyectohotel');
+$con = mysqli_connect('localhost','root','2004','proyectohotel');
 $res = mysqli_query($con, $sql);
 $lista = array();
 while($row = mysqli_fetch_array($res)){
